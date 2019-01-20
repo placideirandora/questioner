bcdd8b5d62344f6f6ca40a06035b7d69f7537f12
Placide IRANDORA <placideirandora@gmail.com>
2019-01-20 18:14:22 +0200

    Added more routes for upcoming meetups and rsvps

--- server/routes/meetUpRoutes.js
+++ server/routes/meetUpRoutes.js
@@ -6,9 +6,16 @@ const meetUpRouter = express.Router();
 
 meetUpRouter.get ('/api/v1/meetups/', meetUpController.getAllMeetUps);
 
+meetUpRouter.get ('/api/v1/meetups/upcoming/', meetUpController.getUpcomingMeetUps);
+
 meetUpRouter.get ('/api/v1/meetups/:id/', meetUpController.getSpecificMeetUp);
 
 meetUpRouter.post ('/api/v1/meetups/', meetUpController.createMeetUp);
 
+meetUpRouter.get ('/api/v1/meetups/:id/rsvps/', meetUpController.getMeetUpRSVP);
+
+meetUpRouter.post ('/api/v1/meetups/:id/rsvps/', meetUpController.createMeetUpRSVP);
+
 module.exports = meetUpRouter;
+
 
