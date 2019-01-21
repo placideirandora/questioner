aea1eb99d22b6e19e5b30b1c2362a0a976340235
Placide IRANDORA <placideirandora@gmail.com>
2019-01-21 09:10:28 +0200

    Improved Code Indent

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,6 +5,7 @@ const userController = require ('../controllers/userController.js');
 const userRouter = express.Router();
 
 userRouter.get ('/api/v1/users/', userController.getAllUsers);
+
 userRouter.get ('/api/v1/users/:id/', userController.getSpecificUser);
 
 userRouter.post ('/api/v1/users/', userController.createUser);
