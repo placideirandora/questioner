4bfba90c0ebb824e53a815fa8fc9b4f2e2649856
Placide IRANDORA <placideirandora@gmail.com>
2019-01-24 18:01:36 +0200

    [Chore] Added Token Verification

---
+++ server/middleware/tokenVerification.js
@@ -0,0 +1,38 @@
+import jwt from 'jsonwebtoken';
+
+module.exports = {
+
+    verifyToken:(req, res, next) => {
+
+        const token = req.headers.authorization;
+
+        if(!token) {
+
+            return res.status(401).send({error:'authentication failed'});
+
+        }
+
+        const split = token.split(' ');
+
+        jwt.verify(split[1],'secret-key',(error,decoded) => {
+
+            if(error) {
+
+                return res.status(401).send({error:'Failed to decode the token'});
+
+            }
+
+            if(decoded){
+
+                req.user=decoded;
+
+                next();
+
+            } else {
+
+                return res.status(401).json({error:'authentication failed'}); 
+
+            }
+        })
+    }
+}
