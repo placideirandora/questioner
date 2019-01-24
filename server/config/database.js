3c3d0127a9b7f36a90e668da052b420d4ae4b36e
Placide IRANDORA <placideirandora@gmail.com>
2019-01-24 17:57:15 +0200

    [Chore] Configured PostgreSQL Database Connection

--- server/config/database.js
+++ server/config/database.js
@@ -1,15 +1,105 @@
-import {Pool} from 'pg';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
 
-const connection = new Pool({
+dotenv.config();
 
-    user: 'postgres',
-    host: 'localhost',
-    database: 'questioner',
-    password: '1234567890',
-    port: 5000,
-
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
 });
 
+const connect = async () => await pool.connect();
+
+const databaseTables = async () => {
+  const admininfo = [
+    process.env.ADMIN_FIRSTNAME,
+    process.env.ADMIN_LASTNAME,
+    process.env.ADMIN_OTHER,
+    process.env.ADMIN_EMAIL,
+    process.env.ADMIN_PHONENUMBER,
+    process.env.ADMIN_USERNAME,
+    process.env.ADMIN_PASSWORD,
+    process.env.IS_ADMIN,
+  ];
 
 
-export default connection;
+const user = `CREATE TABLE IF NOT EXISTS
+users(
+    id SERIAL PRIMARY KEY,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    othername VARCHAR(50),
+    email VARCHAR(50) UNIQUE NULL,
+    phonenumber VARCHAR(20) UNIQUE NULL,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    registered TIMESTAMP default current_timestamp,
+    isadmin BOOLEAN NOT NULL default FALSE
+);`;
+
+
+  const meetup = `CREATE TABLE IF NOT EXISTS
+meetups(
+    id SERIAL PRIMARY KEY,
+    createdon TIMESTAMP default current_timestamp,
+    topic VARCHAR(128) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    happeningon DATE UNIQUE,
+    tags text[],
+    images text[]
+    
+    
+);`;
+
+  const question = `CREATE TABLE IF NOT EXISTS
+questions(
+    id SERIAL PRIMARY KEY,
+    createdon TIMESTAMP default current_timestamp,
+    createdby INTEGER REFERENCES users(id),
+    meetup INTEGER REFERENCES meetups(id),
+    title VARCHAR(50) NOT NULL,
+    body TEXT NOT NULL,
+    upvote INTEGER DEFAULT 0,
+    downvote INTEGER DEFAULT 0
+);`;
+
+  const rsvp = `CREATE TABLE IF NOT EXISTS
+rsvp(
+    id SERIAL PRIMARY KEY,
+    meetup INTEGER REFERENCES meetups(id),
+    userid INTEGER REFERENCES users(id),
+    response VARCHAR(10) NOT NULL
+);`;
+
+  const vote = `CREATE TABLE IF NOT EXISTS
+votes(
+  id SERIAL PRIMARY KEY,
+  userid INTEGER REFERENCES users(id),
+  question INTEGER REFERENCES questions(id),
+  upvote INTEGER DEFAULT 0,
+  downvote INTEGER DEFAULT 0
+);`;
+
+pool.query(user);
+
+pool.query(meetup);
+
+  
+};
+
+//
+
+const dropTables = async () => {
+  const dropAlltables = 'DROP TABLE IF EXISTS rsvp CASCADE';
+
+  const connection = await connect();
+  await connection.query(dropAlltables);
+};
+
+databaseTables();
+
+
+export default pool;
