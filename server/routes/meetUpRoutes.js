e1684f00dd14e6f27f34d27c8aa5db8c1e7e120f
Placide IRANDORA <placideirandora@gmail.com>
2019-01-25 09:45:06 +0200

    [Chore] Reconfigured For Heroku Functionality

--- server/routes/meetUpRoutes.js
+++ server/routes/meetUpRoutes.js
@@ -8,16 +8,27 @@ const meetUpRouter = express.Router();
 
 meetUpRouter.get ('/api/v1/meetups/', tokenVerification.verifyToken, meetUpController.getAllMeetUps);
 
-meetUpRouter.get ('/api/v1/meetups/upcoming/', meetUpController.getUpcomingMeetUps);
+meetUpRouter.get ('/api/v1/meetups/upcoming/', tokenVerification.verifyToken, meetUpController.getUpcomingMeetUps);
 
 meetUpRouter.get ('/api/v1/meetups/:id/', tokenVerification.verifyToken, meetUpController.getSpecificMeetUp);
 
+meetUpRouter.delete ('/api/v1/meetups/:id/', tokenVerification.verifyToken, meetUpController.deleteSpecificMeetUp);
+
 meetUpRouter.post ('/api/v1/meetups/', tokenVerification.verifyToken, meetUpController.createMeetUp);
 
-meetUpRouter.get ('/api/v1/meetups/:id/rsvps/', meetUpController.getMeetUpRSVP);
+//meetUpRouter.get ('/api/v1/meetups/:id/rsvps/', tokenVerification.verifyToken, meetUpController.getMeetUpRSVP);
 
-meetUpRouter.post ('/api/v1/meetups/:id/rsvps/', meetUpController.createMeetUpRSVP);
+meetUpRouter.post ('/api/v1/meetups/:id/rsvps/', tokenVerification.verifyToken, meetUpController.createMeetUpRSVP);
+
+meetUpRouter.post ('/api/v1/meetups/:id/questions/', tokenVerification.verifyToken, meetUpController.createQuestion);
+
+meetUpRouter.get ('/api/v1/meetups/:id/questions/', tokenVerification.verifyToken, meetUpController.getAllQuestions);
+
+meetUpRouter.patch ('/api/v1/meetups/:id/questions/:id/upvote', tokenVerification.verifyToken, meetUpController.upvoteQuestion);
+
+meetUpRouter.post ('/api/v1/meetups/:id/questions/:id/comments', tokenVerification.verifyToken, meetUpController.commentQuestion);
+
+//meetUpRouter.patch ('/api/v1/meetups/:id/questions/:id/downvote', tokenVerification.verifyToken, meetUpController.downvoteQuestion);
 
 export default meetUpRouter;
-
 
