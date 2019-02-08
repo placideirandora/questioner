34d6df801e198a3f26ed7cd822c991522849b7b5
Placide IRANDORA <placideirandora@gmail.com>
2019-02-08 23:00:53 +0200

    [chore] added more dummy data]

--- server/models/dummy.js
+++ server/models/dummy.js
@@ -1,3 +1,31 @@
+const rsvps = [
+    {
+        "meetup": "2",
+        "status": "yes"
+    },
+    {
+        "meetup": "1",
+        "status": "maybe"
+    }
+];
+const questions = [
+    {
+        "id": 1,
+        "meetup": 2,
+        "title": "FELLOWSHIP",
+        "body": "What is the salary of Andela Fellowship?",
+        "upvotes": 0,
+        "downvotes": 0
+    },
+    {
+        "id": 2,
+        "meetup": 1,
+        "title": "ALC",
+        "body": "What is the importance of ALC?",
+        "upvotes": 0,
+        "downvotes": 0
+    }
+];
 const meetups = [
     {
         id: 1,
@@ -8,7 +36,6 @@ const meetups = [
         happeningOn: new Date("2019-02-01").toGMTString(),
         tags: [ "software development", "andela", "web development" ]
     },
-
     {
         id: 2,
         createdOn: new Date().toGMTString(),
@@ -18,7 +45,6 @@ const meetups = [
         happeningOn: new Date("2019-08-01").toGMTString(),
         tags: [ "andela", "fellowship", "assessments" ]
     },
-
     {
         id: 3,
         createdOn: new Date().toGMTString(),
@@ -28,12 +54,38 @@ const meetups = [
         happeningOn: new Date("2019-05-15").toGMTString(),
         tags: [ "JavaScript", "Python", "CSS" ]
     },
-
 ];
-
-const rsvps = [  ];
+const users = [
+    {   
+        "id": 1,
+        "firstname": "Placide",
+        "lastname": "IRANDORA",
+        "othername": "geek",
+        "email": "placideirandora@gmail.com",
+        "phoneNumber": "0786045655",
+        "username": "placideirandora"
+    },
+    {   
+        "id": 2,
+        "firstname": "Innocent",
+        "lastname": "TUYISHIMIRE",
+        "othername": "bienvenu",
+        "email": "innocenttuyishimire@gmail.com",
+        "phoneNumber": "07861885655",
+        "username": "bienvenu" 
+    },
+    {   
+        "id": 3,
+        "firstname": "Fred",
+        "lastname": "MANZI",
+        "othername": "nyakawhite",
+        "email": "fredmanzi@gmail.com",
+        "phoneNumber": "0784579584",
+        "username": "fredmanzi" 
+    },
+];
 
 export default
 {
-    meetups, rsvps
+    meetups, rsvps, users, questions
 }
