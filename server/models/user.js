918463080fab3ee2fbe6bdc5b03c5af3b68c45fc
Placide IRANDORA <placideirandora@gmail.com>
2019-02-08 22:55:09 +0200

    [chore] added user model for capturing new user

---
+++ server/models/user.js
@@ -0,0 +1,15 @@
+
+class user {
+    constructor(id, firstname, lastname, othername, email, phoneNumber, username) {
+      this.id = id,
+      this.firstname = firstname;
+      this.lastname = lastname;
+      this.othername = othername;
+      this.email = email;
+      this.phoneNumber = phoneNumber
+      this.username = username;
+    }
+  }
+
+  export default user;
+
