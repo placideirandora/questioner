42f1c2828c219e295e3fe0f06c49194381884b08
Placide IRANDORA <placideirandora@gmail.com>
2019-02-08 23:00:19 +0200

    [chore] improved the validations

--- server/middleware/validate.js
+++ server/middleware/validate.js
@@ -6,8 +6,7 @@ const userSchema = Joi.object().keys({
   othername: Joi.string().alphanum().min(3).max(50),
   phoneNumber: Joi.number().integer().min(12).required(),
   email: Joi.string().email({ minDomainAtomas: 2 }).required(),
-  username: Joi.string().alphanum().min(3).max(50) .required(),
-  password: Joi.string().alphanum().min(3).max(50) .required(),
+  username: Joi.string().alphanum().min(3).max(50) .required()
 });
 
 const meetupSchema = Joi.object().keys({
@@ -20,21 +19,10 @@ const meetupSchema = Joi.object().keys({
 
 const questionSchema = Joi.object().keys({
   meetupId: Joi.number().integer().required(),
-  createdBy: Joi.number().integer().required(),
-  title: Joi.string().alphanum().min(5).max(50).required(),
+  title: Joi.string().min(3).max(50).required(),
   body: Joi.string().min(5).max(120).required(),
   upvote: Joi.number().integer(),
   downvote: Joi.number().integer(),
-});
-
-const loginSchema = Joi.object().keys({
-  email: Joi.string().email({ minDomainAtomas: 2 }).required(),
-  password: Joi.string().alphanum().min(3).max(50).required(),
-});
-
-const commentSchema = Joi.object().keys({
-  questionId: Joi.number().required(),
-  comment: Joi.string().alphanum().required(),
 });
 
 const rsvpSchema = Joi.object().keys({
@@ -56,11 +44,9 @@ const userParams = Joi.object().keys({
 export default {
   userSchema,
   meetupSchema,
+  rsvpSchema,
   questionSchema,
-  loginSchema,
   questionParams,
   meetupParams,
   userParams,
-  commentSchema,
-  rsvpSchema,
 };
