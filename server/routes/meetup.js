a45a8c46b27450f192cec1d22bfe281b82178edc
Placide IRANDORA <placideirandora@gmail.com>
2019-02-09 15:15:43 +0200

    [chore] added meetup update route with put method

--- server/routes/meetup.js
+++ server/routes/meetup.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get('/upcoming', auth.verifyUser, controller.retrieveUpcomingMeetUps);
 router.post('/', auth.verifyAdmin, controller.createMeetUp);
 router.get('/:id', auth.verifyAdmin, controller.retrieveSpecificMeetUp);
+router.put('/:id', auth.verifyAdmin, controller.updateSpecificMeetUp);
 router.get('/', auth.verifyAdmin, controller.retrieveAllMeetUps);
 router.delete('/:id', auth.verifyAdmin, controller.deleteSpecificMeetUp);
 router.post('/:id/rsvps', auth.verifyUser, controller.submitRSVP);
