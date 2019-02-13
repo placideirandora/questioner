8082f1091eb085e36ed5e5ae3096150e8d6ed4e9
Placide IRANDORA <placideirandora@gmail.com>
2019-02-13 10:52:18 +0200

    [chore] clean coded

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,36 +1,32 @@
-/**
- * Variables
- */
-const signupButton = document.getElementById('signup-button'),
-    loginButton = document.getElementById('login-button'),
-    userForms = document.getElementById('user_options-forms')
+//Variables
 
-/**
- * Add event listener to the "Sign Up" button
- */
-signupButton.addEventListener('click', () => {
-  userForms.classList.remove('bounceRight')
-  userForms.classList.add('bounceLeft')
-}, false)
-
-/**
- * Add event listener to the "Login" button
- */
-loginButton.addEventListener('click', () => {
-  userForms.classList.remove('bounceLeft')
-  userForms.classList.add('bounceRight')
-}, false)
-
-
-/**
- * modal
- */
-
+const signupButton = document.getElementById('signup-button');
+const loginButton = document.getElementById('login-button');
+const userForms = document.getElementById('user_options-forms')
 const modal = document.querySelector(".modal");
 const trigger = document.querySelector("#trigger");
 const closeButton = document.querySelector(".close-button");
 const resetButton = document.querySelector("#reset");
 
+//Event Listners
+
+signupButton.addEventListener('click', () => {
+  userForms.classList.remove('bounceRight')
+  userForms.classList.add('bounceLeft')
+}, false)
+
+loginButton.addEventListener('click', () => {
+  userForms.classList.remove('bounceLeft')
+  userForms.classList.add('bounceRight')
+}, false)
+
+trigger.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+resetButton.addEventListener("click", ()=>{
+  alert("Password Reset Link Has Been Sent To The Email");
+})
+
+//Functions
 
 function toggleModal() {
     modal.classList.toggle("show-modal");
@@ -42,12 +38,7 @@ function windowOnClick(event) {
     }
 }
 
-trigger.addEventListener("click", toggleModal);
-//closeButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
-resetButton.addEventListener("click", ()=>{
-  alert("Password Reset Link Has Been Sent To The Email");
-})
+
 
 
 
