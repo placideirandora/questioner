963899fa95726cf8c06b63d6e145e1c3b8b4b87a
Placide IRANDORA <placideirandora@gmail.com>
2019-03-12 00:29:01 +0200

    [chore] change port number

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use((error, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3500;
 app.listen (PORT, () => { 
   console.log (`Server listening on port: ${PORT}`); 
 });
